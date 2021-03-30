b697160fa71a73a5f6f44364fcc03a98f311ff80
Wendell <wendell_de_lima@hotmail.com>
2021-03-30 17:39:21 -0300

    Desafio FInalizado!!

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,41 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+interface SideBarProps {
+  setSelectedGenreId: (id: number) => void;
+  selectedGenreId: number;
+
+  genres: Array<{
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }>
+}
+
+export function SideBar({ 
+  selectedGenreId, 
+  setSelectedGenreId, 
+  genres 
+}:SideBarProps) {
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
